0ef5e5b4d8aa6d5b83bbbba00f472e5bd984f809
mehedi609 <mehedi609@gmail.com>
2021-02-16 11:53:43 +0600

    project and database seutp.

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+// const connectDB = require('./config/db');
+
+require("dotenv").config();
+
+const app = express();
+
+// Connect to Database
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Database connected successfully.."))
+  .catch((error) => console.error(error));
+
+// bodyParser
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => res.send("API is Running..."));
+
+// Define Routes
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server started on http://localhost:${PORT}`)
+);
